265caddcd16dedb4077df05dc3d9a04a85ccdd8a
Prashant Shishodia <prashant.cse.iitkgp@gmail.com>
2024-09-17 19:27:29 +0530

    replace retrieval from F.cosine_similarity to faiss

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import open_clip
 import pandas as pd
 import time
 import torch
+import torch.nn.functional as F
+import faiss
 
 torch.set_grad_enabled(False)
 
 model, preprocess, tokenizer = None, None, None
 final_df = None
 image_embeddings = None
+faiss_index = None
 similar_products_cached = None
 
 def init_model():
@@ -33,9 +36,13 @@ def init_image_embeddings():
     global image_embeddings
     print('Reading image embeddings...')
     start_time = time.time()
-    image_embeddings = torch.load('image_embeddings_kaggle.pt', map_location=torch.device('cpu'))
-    image_embeddings /= image_embeddings.norm(dim=-1, keepdim=True)
+    image_embeddings = F.normalize(torch.load('image_embeddings_kaggle.pt'), dim=-1).detach().numpy()
     print('Read image embeddings.\nTime Taken: ', time.time() - start_time)
+    
+def init_faiss_index():
+    global faiss_index
+    faiss_index = faiss.IndexFlatIP(512)
+    faiss_index.add(image_embeddings) 
 
 def init_ml():
     global similar_products_cached
@@ -43,6 +50,7 @@ def init_ml():
     
     init_model()
     init_image_embeddings()
+    init_faiss_index()
     similar_products_cached = torch.load('similar_products_cached.pt')
     
 
@@ -55,6 +63,7 @@ assert tokenizer is not None
 assert final_df is not None
 # Assert embeddings
 assert image_embeddings is not None
+assert faiss_index is not None
 assert similar_products_cached is not None
 
 app = Flask(__name__)
@@ -63,6 +72,7 @@ app = Flask(__name__)
 def home():
     return render_template('landingpage.html')
 
+# DEPRECATED : Use faiss_index.search instead. 
 def getTopK(base_embedding, K=100):
     global image_embeddings
     probs = torch.nn.functional.cosine_similarity(image_embeddings, base_embedding.view(1, 512))
@@ -70,15 +80,18 @@ def getTopK(base_embedding, K=100):
     return topk_indices, probs[topk_indices]
 
 def process_query(query):
+    global tokenizer, faiss_index, final_df
     if not query:
         return None, "Query cannot be empty", 400
     
     query_encoding = tokenizer(query)
-    query_embedding = model.encode_text(query_encoding)
-    query_embedding /= query_embedding.norm(dim=-1, keepdim=True)
+    query_embedding = F.normalize(model.encode_text(query_encoding), dim=-1).unsqueeze(0)
 
-    topk_indices, topk_scores = getTopK(query_embedding)
-    products = final_df.iloc[topk_indices.tolist()].to_dict('records')
+    ## faiss_index.search expects batched inputs.
+    topk_indices, topk_scores = faiss_index.search(query_embeddings.detach().numpy(),  100) 
+    topk_indices, topk_scores = topk_indices[0], topk_scores[0]
+
+    products = final_df.iloc[topk_indices].to_dict('records')
     return {"query": query, "products": products, "scores": topk_scores.tolist()}, None, 200
 
 @app.route('/query/<query>')
